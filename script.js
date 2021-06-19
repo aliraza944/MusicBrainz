9bb69fb0014ad2e083bdc23d5072b6ec36d5d303
Ali Raza <alisabri022@gmail.com>
2021-06-19 16:08:36 +0500

    removed all the innerHTML

--- script.js
+++ script.js
@@ -29,25 +29,26 @@ const tableBody = document.getElementById("tbody");
 
 const covers = document.getElementById("covers");
 const modalTitle = document.getElementById("modalTitle");
-const songTitle = document.getElementById("songTitle");
-const artistName = document.getElementById("artistName");
-const albumName = document.getElementById("albumName");
-const genres = document.getElementById("genres");
-const songLength = document.getElementById("songLength");
+const songTitle = document.getElementsByClassName("songTitle")[0];
+const artistName = document.getElementsByClassName("artistName")[0];
+const albumName = document.getElementsByClassName("albumName")[0];
+const genres = document.getElementsByClassName("genres")[0];
+const songLength = document.getElementsByClassName("songLength")[0];
 const note = document.getElementById("note");
-
+const theTitle = document.getElementById("theTitle");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 const modalClose = document.getElementById("modalClose");
 // When the user clicks on <span> (x), close the modal
 function emptyModal() {
-  songTitle.innerHTML = "";
-  artistName.innerHTML = "";
-  albumName.innerHTML = "";
-  genres.innerHTML = "";
-  songLength.innerHTML = "";
-  note.innerHTML = "";
-  covers.innerHTML = "";
+  theTitle.innerText = "";
+  songTitle.innerText = "";
+  artistName.innerText = "";
+  albumName.innerText = "";
+  genres.innerText = "";
+  songLength.innerText = "";
+  note.textContent = "";
+  covers.textContent = "";
 }
 
 span.onclick = function () {
@@ -80,8 +81,8 @@ async function getData() {
     releaseId: [],
     albumId: [],
   };
-  searchResults.innerHTML = "";
-  tableBody.innerHTML = "";
+  searchResults.innerText = "";
+  tableBody.textContent = "";
   search = searchBar.value;
   params = mango.value;
   let url;
@@ -89,23 +90,23 @@ async function getData() {
   switch (params) {
     case "everything":
       data = await fetchAPI(
-        `${baseUrl}recording/?query=artistname:${search}ANDrelease:${search}ANDrecording:${search}&limit=100&dismax=true&fmt=json`
+        `${baseUrl}recording/?query=artistname:${search}ANDrelease:${search}ANDrecording:${search}&dismax=true&fmt=json`
       );
 
       break;
     case "artist":
       url = `${baseUrl}recording/?query=artistname:${search}&fmt=json`;
       data = await fetchAPI(url);
-      console.log(data);
+      // console.log(data);
 
       break;
     case "album":
-      url = `${baseUrl}recording/?query=release:${search}&dismax=true&limit=100&fmt=json`;
+      url = `${baseUrl}recording/?query=release:${search}&dismax=true&fmt=json`;
       data = await fetchAPI(url);
 
       break;
     case "track":
-      url = `${baseUrl}recording/?query=name:${search}&dismax=true&limit=100&fmt=json`;
+      url = `${baseUrl}recording/?query=name:${search}&dismax=true&fmt=json`;
       data = await fetchAPI(url);
 
       break;
@@ -142,29 +143,39 @@ const destructApi = (recordings) => {
 const displayTable = (everythingObj) => {
   searchResults.innerText = `Search Results: ${everythingObj.resluts}`;
 
-  let tr = "";
-
   for (let i = 0; i < Object.keys(everythingObj.artistName).length; i++) {
-    const tr = document.createElement("tr");
+    const button = document.createElement("input");
+    button.type = "button";
+    button.value = "Info";
+    button.className = "btn btn-primary";
+    button.setAttribute("tabindex", `${everythingObj.albumId[i]}`);
+    button.setAttribute("index", `${i}`);
+    button.setAttribute("release", `${everythingObj.releaseId[i]}`);
+    button.onclick = () => {
+      viewDetails(
+        button.getAttribute("tabindex"),
+        button.getAttribute("index"),
+        button.getAttribute("release")
+      );
+    };
+    const row = tableBody.insertRow(i);
+    const srCell = row.insertCell(0);
+    const artistCell = row.insertCell(1);
+    const songCell = row.insertCell(2);
+    const albumCell = row.insertCell(3);
+    const inforCell = row.insertCell(4);
 
-    tr.innerHTML = `<tr>
-    <td>${i}</td>
-    <td>${everythingObj.artistName[i]}</td>
-    <td>${everythingObj.songTitle[i]}</td>
-    <td>${everythingObj.albumTitle[i]}</td>
-    <td tabindex=${everythingObj.albumId[i]} index=${i} release=${everythingObj.releaseId[i]} onclick="viewDetails(this);"><input type="button" name='ali' value="Info"></input></td>
-    </tr>`;
-    tableBody.appendChild(tr);
+    srCell.innerText = `${i + 1}`;
+    artistCell.innerText = `${everythingObj.artistName[i]}`;
+    songCell.innerText = `${everythingObj.songTitle[i]}`;
+    albumCell.innerText = `${everythingObj.albumTitle[i]}`;
+    inforCell.appendChild(button);
   }
 };
 
-async function viewDetails(e) {
+async function viewDetails(tabIndex, index, release) {
   modal.style.display = "block";
-  await displayModal(
-    e.getAttribute("tabindex"),
-    e.getAttribute("index"),
-    e.getAttribute("release")
-  );
+  await displayModal(tabIndex, index, release);
 }
 
 // function to get data to display for the modal
@@ -176,12 +187,11 @@ async function fetchAPI(url, offset = 0) {
     const res = await response.json();
     recordingsData = res.recordings;
 
-    console.log(res.recordings);
+    // console.log(res.recordings);
     if (res.recordings.length < 1) {
       spinner.style.display = "none";
       return recordingsData;
     } else {
-      // await delay(1100);
       spinner.style.display = "block";
 
       return recordingsData.concat(
@@ -190,7 +200,7 @@ async function fetchAPI(url, offset = 0) {
     }
   } catch (error) {
     spinner.style.display = "none";
-    if (error) error.innerHTML = `<h1>something went wrong</h1>`;
+    if (error) error.textContent = `<h1>something went wrong</h1>`;
   }
 }
 
@@ -200,14 +210,14 @@ async function modalDataFetch(url) {
     const res = await response.json();
     return res;
   } catch (error) {
-    if (error) error.innerHTML = `<h1>something went wrong</h1>`;
+    if (error) error.innerText = `<h1>something went wrong</h1>`;
   }
 }
 
 // displayModal function
 
 async function displayModal(albumcover, index, release) {
-  console.log(release);
+  // console.log(release);
   const url = ` https://musicbrainz.org/ws/2/release-group/${albumcover}?inc=ratings+genres&fmt=json`;
   const res = await modalDataFetch(url);
 
@@ -219,41 +229,40 @@ async function displayModal(albumcover, index, release) {
     minutes = Math.floor(everythingObj.songLength[index] / 60000);
     seconds = ((everythingObj.songLength[index] % 60000) / 1000).toFixed(2);
   }
-  console.log(res);
+  // console.log(res);
 
-  //  modal title
-  modalTitle.innerHTML = `<h5>${everythingObj.artistName[index]}- <span id='data'>${everythingObj.songTitle[index]}</span></h5>`;
+  theTitle.innerText = `${everythingObj.artistName[index]}-${everythingObj.songTitle[index]}`;
+
   //song title
-  songTitle.innerHTML = `<h5>Title- <span id='data'>${everythingObj.songTitle[index]}</span></h5>`;
+  songTitle.innerText = `${everythingObj.songTitle[index]}`;
   //artist name
-  artistName.innerHTML = `<h5>Artis- <span id='data'>${everythingObj.artistName[index]}</span></h5>`;
+  artistName.innerText = `${everythingObj.artistName[index]}`;
   // album name
-  albumName.innerHTML = `<h5>Album- <span id='data'>${everythingObj.albumTitle[index]}</span></h5>`;
+  albumName.innerText = `${everythingObj.albumTitle[index]}`;
   // check if the genres property exits
   if (res.genres.length != 0) {
-    genres.innerHTML = `<h5>Genres- <span id='data'>${res.genres[0].name}</span></h5>`;
+    genres.innerText = `${res.genres[0].name}`;
   } else {
-    genres.innerHTML = `<h5>Genres-</h5>`;
+    genres.innerText = ``;
   }
   // songLength
-  songLength.innerHTML = `<h5>Length- <span id='data'>${parseInt(
-    minutes
-  )} : ${parseInt(seconds)}</span></h5>`;
+  songLength.innerText = `${parseInt(minutes)} : ${parseInt(seconds)}`;
   // rating value
 
   if (res.rating.value != null) {
     for (i = 0; i < parseInt(res.rating.value); i++) {
-      const p = document.createElement("span");
-      p.innerHTML = `<i class="far fa-star"></i>`;
+      const p = document.createElement("i");
+      p.className = "far fa-star";
+
       note.appendChild(p);
     }
   } else {
-    note.innerHTML = `<span id="data">Not Rated</span>`;
+    note.innerText = ``;
   }
   const urlPoster = `http://coverartarchive.org/release/${release}`;
-  //delay(2000);
+
   const imageData = await modalDataFetch(urlPoster);
-  console.log(imageData);
+  // console.log(imageData);
 
   displayImage(imageData);
 }
@@ -263,22 +272,52 @@ const displayImage = (imageData) => {
   if (imageData && imageData.images) {
     for (let i = 0; i < imageData.images.length; i++) {
       nextImage = i + 1;
-      const img = document.createElement("img");
-
       const imageWrapper = document.createElement("div");
       imageWrapper.className = "row";
       if (nextImage === imageData.images.length) {
-        imageWrapper.innerHTML = `<div class="col"><img class="mt-3" src=${imageData.images[i].image} alt="" /></div>`;
+        const innerDiv = document.createElement("div");
+        innerDiv.className = "col";
+        const image = document.createElement("img");
+        image.className = "mt-3";
+        image.src = imageData.images[i].image;
+        innerDiv.appendChild(image);
+        imageWrapper.appendChild(innerDiv);
 
         covers.appendChild(imageWrapper);
       } else {
-        imageWrapper.innerHTML = `<div class="col"><img class="mt-3" src=${imageData.images[i].image} alt="" /></div><div class="col"><img class="mt-3" src=${imageData.images[nextImage].image} alt="" /></div>`;
+        // creat the column divs
+        const col1 = document.createElement("div");
+        const col2 = document.createElement("div");
+        // asign them their classes
+        col1.className = "col";
+        col2.className = "col";
+        // create the images for bot of the columns
+
+        const firstColumnImage = document.createElement("img");
+        firstColumnImage.className = "mt-3";
+        firstColumnImage.src = imageData.images[i].image;
+
+        const secondColumnImage = document.createElement("img");
+        secondColumnImage.className = "mt-3";
+        secondColumnImage.src = imageData.images[nextImage].image;
+
+        //  append them into the divs
+
+        col1.appendChild(firstColumnImage);
+        col2.appendChild(secondColumnImage);
+
+        // apend the into the iamge imageWrapper
+
+        imageWrapper.appendChild(col1);
+        imageWrapper.appendChild(col2);
 
         covers.appendChild(imageWrapper);
       }
       i = nextImage;
     }
   } else {
-    covers.innerHTML = `<h1>image not found</h1>`;
+    const errormsg = document.createElement("h1");
+    errormsg.innerText = "image not found";
+    covers.appendChild(errormsg);
   }
 };
